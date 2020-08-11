8b23a151d4a8b47a7fbfc16b26890f98cada6f3f
yuta-eng-prg <horii6773@gmail.com>
2020-08-11 20:26:04 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, uniqueness: { case_sensitive: true }, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-  validates :password, length: { minimum: 6 }, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+  validates :email, uniqueness: { case_sensitive: true }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/ }
 
   with_options presence: true do
     validates :nickname, uniqueness: { case_sensitive: true }
-    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
-    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
   end
-
 end
