5a1f362744cbc5bab00654ae1f44e1699d977eb2
yuta-eng-prg <horii6773@gmail.com>
2020-08-11 12:30:58 +0900

    バリデーションの設定の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :password, length: { minimum: 6 }, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+
   with_options presence: true do
     validates :nickname, uniqueness: true
-    validates :email, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-    validates :password, length: { minimum: 6 }, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
     validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
     validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
     validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
