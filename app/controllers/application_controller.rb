54bbe0d147d2c2e2c73e2b70cd389c2a9db055c7
yuta-eng-prg <horii6773@gmail.com>
2020-08-12 05:54:30 +0900

    devise_parameter_sanitizerメソッドの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:password])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname,
+      :password,
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :birthday
+      ])
   end
 
   private
