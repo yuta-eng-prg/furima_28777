de75e0921b21408f9a832459bcbdf25d188acc3f
yuta-eng-prg <horii6773@gmail.com>
2020-08-12 18:34:12 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,29 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :description,
+      :category,
+      :status,
+      :shipping_fee_burden,
+      :shipping_region,
+      :days_until_shipping
+    ).merge(user_id: current_user.id)
+  end
+
 end
