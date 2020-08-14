71b553c5bca0e1e1366f70ec4d2c264c1e8bcdba
yuta-eng-prg <horii6773@gmail.com>
2020-08-14 15:24:05 +0900

    商品情報編集ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,17 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    
+  end
+
   private
 
   def item_params
