9a16ad8f1bc6f73fc3e98022fe40cc4ba98aca8a
yuta-eng-prg <horii6773@gmail.com>
2020-08-16 11:04:43 +0900

    クレジット決済機能の導入

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,34 @@
 class PurchasesController < ApplicationController
 
   def index
-    
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    
+    @purchase = Purchase.new(user_id: purchase_params[:user_id], item_id: purchase_params[:item_id])
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  
+  def purchase_params
+    params.permit(:user_id, :item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    item = Item.find(purchase_params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items, except: [:index]
-  resources :purchases, only: [:index, :create]
+  resources :items, except: [:index] do
+    resources :purchases, only: [:index, :create]
+  end
 end
